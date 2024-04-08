5003d31f0bb98cd862d91488e04ca2ddefe59dff
Yaron Peres <yaronp16@gmail.com>
2024-04-08 18:56:15 +0200

    first

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,21 +1,37 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as copiedGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: copiedGoal[];
   onDeleteGoal: (id: number) => void;
 };
 function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no course goals yet</InfoBox>;
+  }
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You have to many goals.
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
